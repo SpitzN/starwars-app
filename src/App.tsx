f6da7d3976440a662aeb2f8645324525034a7b97
SpitzN <nadav.spitzer@gmail.com>
2024-05-27 21:01:44 +0300

    animated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import EntityListPage from "./pages/EntityListPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import EntityPage from "./pages/EntityPage";
+import { AnimatePresence } from "framer-motion";
+import AnimatedRoute from "./components/AnimatedRoute";
 
 const App: React.FC = () => {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<SearchPage />} />
-      <Route path="/entities/:type" element={<EntityListPage />} />
-      <Route path="/entity/*" element={<EntityPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <AnimatedRoute>
+              <SearchPage />
+            </AnimatedRoute>
+          }
+        />
+        <Route
+          path="/entities/:type"
+          element={
+            <AnimatedRoute>
+              <EntityListPage />
+            </AnimatedRoute>
+          }
+        />
+        <Route
+          path="/entity/*"
+          element={
+            <AnimatedRoute>
+              <EntityPage />
+            </AnimatedRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <AnimatedRoute>
+              <NotFoundPage />
+            </AnimatedRoute>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
